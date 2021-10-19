507145bec8e6de8798760c013f6a7a5527126014
Milykh Svetozar <svt2404@gmail.com>
2021-10-19 22:06:48 +0300

    Create Main.java

---
+++ src/lesson11/Main.java
@@ -0,0 +1,11 @@
+package lesson11;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(RomanNumber.ONE);
+        System.out.println(RomanNumber.FIVE.getValue());
+        System.out.println(RomanNumber.ONE.getValue() + RomanNumber.TEN.getValue());
+
+
+    }
+}

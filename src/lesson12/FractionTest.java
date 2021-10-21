32abd8a9550413c6afb8069045407bd4eb8017b8
Milykh Svetozar <svt2404@gmail.com>
2021-10-21 22:04:02 +0300

    Create FractionTest.java

---
+++ src/lesson12/FractionTest.java
@@ -0,0 +1,31 @@
+package lesson12;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FractionTest {
+    @Test
+    @Order(2)
+    public void simplifyTest() {
+        Fraction value = new Fraction(45, 10);
+        Fraction actual = value.simplify();
+        Fraction expected = new Fraction(9, 2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @Order(1)
+    public void plusOperationTest() {
+        Fraction first = new Fraction(1, 2);
+        Fraction second = new Fraction(3, 5);
+        Fraction actual = first.plus(second);
+        Fraction expected = new Fraction(11, 10);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @Order(0)
+    public void test() {
+
+    }
+}
